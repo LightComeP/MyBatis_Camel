63848050127908335e24f3f3b08dc5f5b7fa90d9
LightComeP <61652477+LightComeP@users.noreply.github.com>
2021-11-03 17:09:25 +0900

    [신규] 신규PGM 카멜 케이스 배포
    배포 일자 : 21.11.03
    상세 내역 : -

---
+++ camel.js
@@ -0,0 +1,70 @@
+document.addEventListener("load", function () {
+});
+var fGetComf;
+var fTextCamelCaseparsing;
+fGetComf = function (comfid) {
+    return document.getElementById(comfid);
+};
+////////////////////////////////////////////
+//선언
+////////////////////////////////////////////
+fTextCamelCaseparsing = function () {
+    var colText = fGetComf('ta_col').value; //컬럼명
+    var colCmText = fGetComf('ta_colCommand').value; //컬럼커멘드
+    var gubunText = fGetComf('ibx_gubun').value; //구분자
+    var objresultDiv = fGetComf('resultDiv');
+    var maxLength = 0;
+    document.cookie = 'colText=' + colText;
+    var colArr = colText.split(gubunText); //컬럼 나누기
+    var colCmArr = colCmText.split(gubunText);
+    var resultColArr = [];
+    var resultColCmArr = [];
+    var resultText = "";
+    document.cookie = 'col=' + colText;
+    //////////////////////////////////////////////
+    //colArr 생성
+    for (var _i = 0, colArr_1 = colArr; _i < colArr_1.length; _i++) { // 
+        var col = colArr_1[_i];
+        var colflag = true; // flag 선언
+        col.replaceAll(" ", ""); // 공백 제거
+        if (col == "") {
+            continue; // 공백이라면 포함 X
+        }
+        col = col.toLowerCase(); // 소문자로 변환
+        while (colflag) {
+            var ui = col.indexOf("_"); //UpperIndex
+            if (ui < 0) {
+                colflag = false;
+                continue;
+            }
+            col = col.slice(0, ui) + col.slice(ui + 1, ui + 2).toUpperCase() + col.slice(ui + 2);
+        }
+        if (col.length > maxLength) {
+            maxLength = col.length;
+            var spaceLength = ((maxLength / 4) + 1);
+        }
+        resultColArr.push(col);
+        return null;
+    }
+    //////////////////////////////////////////////
+    //colCmArr 생성
+    for (var i = 0; i < colCmArr.length; i++) { //colCmArr 생성
+        resultColCmArr[i] = colCmArr[i].replaceAll(" ", "");
+    }
+    //////////////////////////////////////////////
+    //노출 데이터 생성
+    for (var i = 0; i < resultColArr.length; i++) {
+        var spaceString = '';
+        var colLength = resultColArr[i].length;
+        Math.ceil(colLength);
+        for (var j = 0; j <= spaceLength - Math.ceil(colLength / 4); j++) {
+            debugger;
+            spaceString += '\t';
+        }
+        if (resultColCmArr[i] == null || resultColCmArr[i] == 'undefined') {
+            resultColCmArr[i] = '';
+        }
+        resultText += (resultColArr[i] + spaceString + resultColCmArr[i] + '\n');
+    }
+    fGetComf('resultDiv').value = resultText;
+};
